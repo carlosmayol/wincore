0c664ca23805313e76ab52c87815f4fd83c666b5
Carlos Mayol (MSFT) <carlosmayol@users.noreply.github.com>
2021-02-01 12:34:06 -0500

    Create s2d-cleandrives.ps1

---
+++ s2d-cleandrives.ps1
@@ -0,0 +1,19 @@
+# Fill in these variables with your values
+$ServerList = "Cluster2016-1", "Cluster2016-2", "Cluster2016-3", "Cluster2016-4"
+
+Invoke-Command ($ServerList) {
+    Update-StorageProviderCache
+    Get-StoragePool | ? IsPrimordial -eq $false | Set-StoragePool -IsReadOnly:$false -ErrorAction SilentlyContinue
+    Get-StoragePool | ? IsPrimordial -eq $false | Get-VirtualDisk | Remove-VirtualDisk -Confirm:$false -ErrorAction SilentlyContinue
+    Get-StoragePool | ? IsPrimordial -eq $false | Remove-StoragePool -Confirm:$false -ErrorAction SilentlyContinue
+    Get-PhysicalDisk | Reset-PhysicalDisk -ErrorAction SilentlyContinue
+    Get-Disk | ? Number -ne $null | ? IsBoot -ne $true | ? IsSystem -ne $true | ? PartitionStyle -ne RAW | % {
+        $_ | Set-Disk -isoffline:$false
+        $_ | Set-Disk -isreadonly:$false
+        $_ | Clear-Disk -RemoveData -RemoveOEM -Confirm:$false
+        $_ | Set-Disk -isreadonly:$true
+        $_ | Set-Disk -isoffline:$true
+    }
+    Get-Disk | Where Number -Ne $Null | Where IsBoot -Ne $True | Where IsSystem -Ne $True | Where PartitionStyle -Eq RAW | Group -NoElement -Property FriendlyName | Sort -Property PsComputerName, Count
+
+}
